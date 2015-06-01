af386346eb11b217f68a252014d510459eec18c0
jwbats <jwbats@gmail.com>
2015-06-01 21:23:44 +0200

    Initial commit.

---
+++ Log4NetSandbox/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using log4net;
+using log4net.Config;
+
+namespace Log4NetSandbox
+{
+	public partial class Default : System.Web.UI.Page
+	{
+
+		public static readonly ILog logger = LogManager.GetLogger("FirstLogFileAppender");
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			XmlConfigurator.Configure();
+
+			logger.Debug("..This is Debug Message...");
+
+			logger.Error("..This is Error Message...");
+
+			logger.Fatal("..This is Fatal Message...");
+
+			logger.Info("..This is Information Message...");
+		}
+	}
+}
---
+++ Log4NetSandbox/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Log4NetSandbox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Log4NetSandbox")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f4eebcbf-9005-4a87-9770-e3d9a23c3479")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
